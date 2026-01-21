7708bd32d5137189fb15f5031f85a4f7ecd3923b
Adscular <team@adscular.agency>
2026-01-21 21:42:40 +0500

    Add FitGym project files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/membership')
+def membership():
+    return render_template('membership.html')
+
+@app.route('/contact', )
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=5000)
